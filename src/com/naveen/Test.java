5fc92db33e93ad2b50263c6dca5f3ff59c326769
NaveenKumarJadi <naveenkumarjadi2005@gmail.com>
2023-07-08 10:06:01 +0530

    Initial Commit

---
+++ src/com/naveen/Test.java
@@ -0,0 +1,21 @@
+package com.naveen;
+
+abstract class Person 								
+{
+	abstract void eat();
+}
+
+class Test 											
+{
+	public static void main(String[] args) {
+		Person p = new Person() 
+		{
+			void eat() 
+			{
+				System.out.println("nice fruits");
+			}
+		};
+		p.eat();
+	}
+
+}
