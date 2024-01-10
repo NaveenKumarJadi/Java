1f608a09f04a247e4922520650c3604d473da720
NaveenKumarJadi <naveenkumarjadi2005@gmail.com>
2024-01-10 06:53:00 +0530

    Average of 2 Numbers

---
+++ 01-AverageOfTwoNumbers/src/com/naveen/MyClass.java
@@ -0,0 +1,14 @@
+package com.naveen;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+
+		int num1 = 8, num2 = 4;
+
+		int avg = (num1 + num2) / 2;
+
+		System.out.println("Average of : " + num1 + " + " + num2 + " = " + avg);
+	}
+
+}
